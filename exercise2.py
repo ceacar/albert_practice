fd062bfe20a0d719602ba71df2b74a2643885ebe
ceacar <alex.yonder@yahoo.com>
2017-05-25 17:38:53 -0400

    added comments for exercise2

--- exercise2.py
+++ exercise2.py
@@ -5,7 +5,7 @@
     #LETTERS 10
     #DIGITS 3
 
-
+#>>>this "function_name" is not a proper function name, it should be character_classifier.
 def function_name(s):
     d=l=0
     for c in s:
@@ -15,13 +15,16 @@ def function_name(s):
             l=l+1
         else:
             pass
+    #>>>why are you returning s which is the input of this function. i assume you want to return the d, l to the outside
     return s
+
+#>>these two doesn't have l and d defined ( they are defined in the function above, but not defined in the global scope
 print("Letters", l)
 print("Digits", d)
 
 
 
-s = raw_input()
+s = "hello world! 123"
 classified_result = character_classifier(s)
 print "LETTERS", classified_result["LETTERS"]
 print "DIGITS", classified_result["DIGITS"]
