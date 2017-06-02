c0e48470b71e310e23a4c006f84cbb61362d0cfa
albert <alex.yonder@yahoo.com>
2017-06-02 18:51:31 -0400

    how to define d and l? #NameError: name 'd' is not defined

--- exercise2.py
+++ exercise2.py
@@ -6,7 +6,7 @@
     #DIGITS 3
 
 #>>>this "function_name" is not a proper function name, it should be character_classifier.
-def function_name(s):
+def character_classifier(s):
     d=l=0
     for c in s:
         if c.isdigit():
@@ -16,15 +16,6 @@ def function_name(s):
         else:
             pass
     #>>>why are you returning s which is the input of this function. i assume you want to return the d, l to the outside
-    return s
-
-#>>these two doesn't have l and d defined ( they are defined in the function above, but not defined in the global scope
-print("Letters", l)
-print("Digits", d)
-
-
-
-s = "hello world! 123"
-classified_result = character_classifier(s)
-print "LETTERS", classified_result["LETTERS"]
-print "DIGITS", classified_result["DIGITS"]
+    return d, l
+print d, l
+#>>these two doesn't have l and d defined ( they are defined in the function above, but not defined in the global
