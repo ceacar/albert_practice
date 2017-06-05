c5ff2397adbcc3b97188729fb4e0916e21a111b2
ceacar <alex.yonder@yahoo.com>
2017-06-04 20:08:14 -0400

    you need to put d, l into a dictionary for print to pick up correctly

--- exercise2.py
+++ exercise2.py
@@ -14,17 +14,22 @@ def character_classifier(s):
             l=l+1
         else:
             pass
-    #you need to convert d and l to int by using  int(d), int(l)
-    return d, l
+    #you need to convert d and l to string by using  str(d), str(l)
+    #also you need to put these two values into a dictionary. lookup python dictionary
+    return str(d), str(l)
 
 #you need to remove this line, cause there are no d, l defined in outer scope, system doesn't know who are they, and i m not sure what are you trying to do here
-print d, l
 
 
 #DO NOT DELETE
 #YOU NEED THESE LINES TO RUN FOR THE RESULT
+
+#this is the string that you will run through your function character_classifier
 s = "hello world! 123"
+#this is the where you run above string
 classified_result = character_classifier(s)
+#you are printing how many letter are there by look up a dictionary with key word "LETTERS"
 print "LETTERS", classified_result["LETTERS"]
+#you are printing how many digits are there by look up a dictionary with key word "DIGITS"
 print "DIGITS", classified_result["DIGITS"]
 
