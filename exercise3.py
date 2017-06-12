8ec69abd274e6512947680d5dacf1ec3ccb2d8fb
albert <alex.yonder@yahoo.com>
2017-06-12 19:56:28 -0400

    write test into the file and then print it

--- exercise3.py
+++ exercise3.py
@@ -1,1 +1,11 @@
 #you need to write a function that can write a string "test" to a file then read it and print out the content of the file
+
+
+file = open("math.py","w")
+
+file.write("test")
+
+file.close()
+
+file = open("math.py","r")
+print file.readlines()
