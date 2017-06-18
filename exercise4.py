d17953493824a78521c8454352400dc0d75c0660
albert <alex.yonder@yahoo.com>
2017-06-18 16:09:08 -0400

    error in print part

--- exercise4.py
+++ exercise4.py
@@ -1,3 +1,27 @@
 #1. understand what's public key what's private key
 #2. write any string to a file
 #3. use your host's public key to encrypt a file and then decrypt it and print it out
+
+
+choice = ''
+message = ''
+choice = ''
+
+while choice != 0:
+    choice = input("\nEnter 1 to encrypt and 2 to decrypt")
+
+    if choice == '1':
+        message = input("\nEnter Message: ")
+        for i in range(0, len(message)):
+            result = result + chr(ord(message[i] - 2)
+
+        print(result + '\n\n')
+        result = ''
+
+    elif choice == '2'
+        message = input("\nEnter Message: ")
+        for i in range(0,len(message)):
+            result = result + chr(ord(message[i]) + 2)
+
+        print(result + '\n\n')
+        result = ''
