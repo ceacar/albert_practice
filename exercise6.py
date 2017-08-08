da00e24068c09726a1e5bc0d873d76aee60842f2
ceacar <alex.yonder@yahoo.com>
2017-08-08 18:38:20 -0400

    adding exercise6

---
+++ exercise6.py
@@ -0,0 +1,10 @@
+#you have input data in the file "stock_input.data"
+
+#read from this file
+#calculate how many rows are in this data file
+#calculate how many rows each symbol
+#calculate average price of each symbol
+#calculate total dollars have been trades
+#calculate total dollar amount for each symbol
+#calcualte each symbol's variance
+
