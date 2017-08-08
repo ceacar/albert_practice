f1405e610e07d5594eb8be54321c4d764a56cf93
ceacar <alex.yonder@yahoo.com>
2017-08-07 21:01:31 -0400

    assignment 5

---
+++ exercise5.py
@@ -0,0 +1,9 @@
+#give you string "abcba"
+#you need to find out if it is symmetrical
+
+def isSymmetrical(input_str):
+    #implement here
+
+#if you are given "afdsagdabcbaasfsd"
+def findAllSymmetrical(input_str):
+    return
