ff43f7571f01daae86a700ff1f12b2a349af54ee
ceacar <alex.yonder@yahoo.com>
2017-08-08 18:38:06 -0400

    adding exercise10

---
+++ exercise10.py
@@ -0,0 +1,12 @@
+#linked list
+#https://en.wikipedia.org/wiki/Linked_list
+#implement a class called "Node" to form a linked list
+#it needs to have a function called next() to navigate to the next node and a function called value() to get the value of it self
+
+
+class Node:
+    def __init__(value, next_node):
+        return
+    def next(self):
+        return
+    def value(self):
