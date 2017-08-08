259535c4ca985f7c0100bbe179d0a4e006b76499
ceacar <alex.yonder@yahoo.com>
2017-08-08 17:37:19 -0400

    guided albert step by step of the solution.

--- exercise5.py
+++ exercise5.py
@@ -1,9 +1,48 @@
 #give you string "abcba"
 #you need to find out if it is symmetrical
 
+
+
+def return_demo():
+    print 1
+    print 2
+    return lambda x: 2*x
+    print 3
+
+#print return_demo()
+
+
 def isSymmetrical(input_str):
     #implement here
+    #compare first character and last character?
+    #compare 2nd and n-1
 
-#if you are given "afdsagdabcbaasfsd"
-def findAllSymmetrical(input_str):
-    return
+
+
+    #step1. need to know the length of string
+
+    #step2. compare the first and last
+
+    #step3. compare the 2nd and n-1
+
+    #stepn. until the mid point
+
+    # use for loop to do step2-> stepn
+
+
+    #if the input is "abcba"
+
+    #1. compare the 1 and 5
+    #2. compare the 2 and 4
+
+
+    #if the input is "abcdefghijklmnopqrstuvwxyzyxwvutsrqponmlkjihgfedcba"
+
+    #1. xrange(start,end)
+    #2. len(string_input)
+    #3. string_input[n]
+    #4. for loop
+
+
+isSymmetrical("abcdefghijklmnopqrstuvwxyzyxwvutsrqponmlkjihgfedcba")
+
