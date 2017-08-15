212f38e3baf70b3cc3cb3b031799be5860552175
albert <alex.yonder@yahoo.com>
2017-08-14 22:23:53 -0400

    adding 7th exercise for some practice with public school data

---
+++ exercise7.py
@@ -0,0 +1,13 @@
+#dataset is named 2014-15_Graduation_Outcomes_-_Ethnicity.csv
+#need results as follows:
+    #graduation student total
+    #graduation student every race percentage
+    #total graduation student by school
+    #total graduation student by race
+    #graduation student by school percentage
+    #graduation student by school by race percentage
+    #overall average year it took for a student to graduate
+    #average year it took to graduate by race
+    #average year it took to graduate by school
+    #graduation school by school by race by year percentage(want to see the shift of race percentage over the years)
+
