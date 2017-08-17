b1edd23eb566ad045a70ddfb6881014cc7202f08
albert <alex.yonder@yahoo.com>
2017-08-17 15:58:36 -0400

    halfway

--- exercise7.py
+++ exercise7.py
@@ -10,4 +10,57 @@
     #average year it took to graduate by race
     #average year it took to graduate by school
     #graduation school by school by race by year percentage(want to see the shift of race percentage over the years)
+import pprint
+student_data = open('./2014-15_Graduation_Outcomes_-_Ethnicity.csv','r')
+#student_data = open('./temp.csv','r')
+first_line = student_data.readline();
+# use for loop to get a dict
+student_dict = {}
+race_dict = {}
+counter = 0
+for line in student_data:
+    lines = line.rstrip().split(",")
+    counter = counter + 1
+    code = lines [0]
+    school = lines [1]
+    years = lines [2]
+    category = lines [3]
+    ethnicity = lines [4]
+    #if ethnicity not in ethnicity_set:
+    #    print lines
+    #    print ethnicity
+    #    ethnicity_set.append(ethnicity)
 
+    if  code in student_dict:
+        student_dict[code].append((school,years,category,ethnicity))
+    else:
+        student_dict[code] = [(school,years,category,ethnicity)]
+    if ethnicity in race_dict:
+        race_dict[ethnicity].append((code,school,years,category))
+    else:
+        race_dict[ethnicity] = [(code,school,years,category)]
+
+#pprint.pprint (student_dict)
+
+#print sum(1 for _ in student_data)
+print counter
+        #(code,school,years,category) = temp
+
+#race_dict: {'black':[()
+#print len(race_dict[ethnicity])
+percentagetemp = {}
+#def percentage(percent):
+    #'''{
+    #    'black':[(),(),()] ,
+    #    'white':[(),(),()] ,
+    #}'''
+    #num = 0
+    #num = len(race_dict[ethnicity])
+        #print num
+
+res = {}
+for k,v in race_dict.iteritems():
+    #import pdb
+    #pdb.set_trace()
+    res[k] = len(v)
+    print res[k]
